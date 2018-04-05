906de03168bfaa34ad606a64744bc82200d2e42c
Endri Gjiri <egjiri@gmail.com>
2018-04-04 23:21:31 -0400

    Fix bugs with bash-completion command

--- executable/bash-completion.go
+++ executable/bash-completion.go
@@ -1,12 +1,22 @@
 package executable
 
 import (
+	"io/ioutil"
+	"log"
+
 	"github.com/egjiri/cliff/cli"
 )
 
 func init() {
 	cli.AddRunToCommand("bash-completion", func(cmd cli.Command, args []string) {
-		path := cmd.Flag("output").Value.String()
-		cli.GenerateBashCompletionFile(path)
+		yamlConfigFilePath := cmd.Flag("file").Value.String()
+		yamlConfigContent, err := ioutil.ReadFile(yamlConfigFilePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		cli.Configure(yamlConfigContent)
+
+		outputPath := cmd.Flag("output").Value.String()
+		cli.GenerateBashCompletionFile(outputPath)
 	})
 }
